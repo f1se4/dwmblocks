a090bdbbba5a193ba4828a0519d4db0ecf351f46
f1se4 <sergi.garcia.munoz@gmail.com>
2022-02-03 22:27:32 +0100

    full status line

--- config.h
+++ config.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval(s)*/	/*Update Signal*/
-	{"",	    "sb-taskw",	          300,               11},
-	{"",	    "sb-cpu",		          10,	               18}, 
-	{"",	    "sb-volume",	        1,	               10},
-	{"",	    "sb-internet",	      5,               	  4},
-	{"",	    "sb-battery2",	      5,                	3},
-	{"",	    "sb-clock",	          60,               	1},
-	{"",	    "sb-moonphase",	      18000,            	17}, 
-	{"",	    "sb-help-icon",     	0,                	15},
+	{"",	    "sb-taskw",	          60,                  10},
+	{"",	    "sb-cpu",		          10,	                 11}, 
+	{"",	    "sb-volume",	        1,	                 12},
+	{"",	    "sb-internet",	      5,                	 13},
+	{"",	    "sb-battery2",	      5,                   14},
+	{"",	    "sb-clock",	          60,                  15},
+	{"",	    "sb-moonphase",	      18000,             	 16}, 
+	{"",	    "sb-help-icon",     	0,                   17},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
